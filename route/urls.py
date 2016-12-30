b60bd4de95cc8149b4b1084ac54282b2d289a48b
vicky <lch82327@gmail.com>
2016-12-30 15:33:30 +0800

    get basic service info

---
+++ route/urls.py
@@ -0,0 +1,4 @@
+# service control
+get_sc_sequence = '/service-control/sequence'
+get_sc_priority = '/service-control/priority'
+get_sc_status = '/service-control/status'
--- services-control.py
+++ services-control.py
@@ -1,3 +1,5 @@
+import json
+from webob import Response
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
@@ -10,6 +12,7 @@ from ryu.lib.packet import ether_types
 
 from route import urls
 from helper import ofp_helper
+from config import service_settings
 
 service_control_instance_name = 'service_control_api_app'
 
@@ -24,8 +27,27 @@ class ServiceControl(app_manager.RyuApp):
                       {service_control_instance_name: self})
 
 
+
 class ServiceControlController(ControllerBase):
     def __init__(self, req, link, data, **config):
         super(ServiceControlController, self).__init__(req,
                                                        link, data, **config)
         self.service_control_spp = data[service_control_instance_name]
+
+    @route('service-control', urls.get_sc_sequence, methods=['GET'])
+    def get_sc_sequence(self, req, **kwargs):
+        dic = service_settings.service_sequence
+        body = json.dumps(dic)
+        return Response(status=200, content_type='application/json', body=body)
+
+    @route('service-control', urls.get_sc_priority, methods=['GET'])
+    def get_sc_priority(self, req, **kwargs):
+        dic = service_settings.service_priority
+        body = json.dumps(dic)
+        return Response(status=200, content_type='application/json', body=body)
+
+    @route('service-control', urls.get_sc_status, methods=['GET'])
+    def get_sc_status(self, req, **kwargs):
+        dic = service_settings.service_status
+        body = json.dumps(dic)
+        return Response(status=200, content_type='application/json', body=body)
